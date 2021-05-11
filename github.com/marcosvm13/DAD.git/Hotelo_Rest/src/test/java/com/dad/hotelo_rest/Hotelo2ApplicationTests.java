fb9b53c5fc1b7669d244980116456e796ac1eade
calderon95 <47868021+calderon95@users.noreply.github.com>
2021-05-11 14:30:38 +0200

    Api Rest

---
+++ github.com/marcosvm13/DAD.git/Hotelo_Rest/src/test/java/com/dad/hotelo_rest/Hotelo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dad.hotelo_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hotelo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
